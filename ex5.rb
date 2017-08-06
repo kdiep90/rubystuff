b617e72b3ff10e7ac00b835d0b9e0a923b547515
kdiep90 <diep90@gmail.com>
2017-08-05 21:10:38 -0400

    Relearning Ruby Exercise 5

---
+++ ex5.rb
@@ -0,0 +1,15 @@
+name = 'Kevin Diep'
+age = 27 #as of 8/5/2017
+height = 70 #inches
+weight = 165 #lbs
+eyes = 'Brown'
+teeth = 'White'
+hair = 'Black'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on how much coffee he's had."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
