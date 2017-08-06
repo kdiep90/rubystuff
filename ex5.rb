84ba94292d9626a11cab212df5ed1b1b4f3e7a16
kdiep90 <diep90@gmail.com>
2017-08-05 21:16:04 -0400

    Exercise 5: trying other variables

--- ex5.rb
+++ ex5.rb
@@ -5,10 +5,12 @@ weight = 165 #lbs
 eyes = 'Brown'
 teeth = 'White'
 hair = 'Black'
+height_in_centimeters = height * 2.54
+weight_in_kilograms = weight * 0.453592
 
 puts "Let's talk about #{name}."
-puts "He's #{height} inches tall."
-puts "He's #{weight} pounds heavy."
+puts "He's #{height} inches, or #{height_in_centimeters} centimeters, tall."
+puts "He's #{weight} pounds, or #{weight_in_kilograms} kilograms, heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on how much coffee he's had."
