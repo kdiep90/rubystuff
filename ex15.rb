b7baa47bb241e2579cd9630663b3012c53386fe5
kdiep90 <diep90@gmail.com>
2017-08-06 21:44:08 -0500

    Ex15 correcting some comments

--- ex15.rb
+++ ex15.rb
@@ -1,5 +1,5 @@
 filename = ARGV.first # assigns a variable to the first argument passed in the shell
-txt = open(filename) # assigns a variable to the content of whatever is in the file called
+txt = open(filename) # assigns a variable to whatever is in the file called
 
 puts "Here's your file #{filename}:"
 print txt.read # reads the content of the sample file
@@ -7,6 +7,6 @@ print txt.read # reads the content of the sample file
 print "Type the filename again: "
 file_again = $stdin.gets.chomp #assigns file name given by the user to a variable
 
-txt_again = open(file_again) # assigns a variable to the content of the file given by the user (which may or may not be the same as the first file called)
+txt_again = open(file_again) # assigns a variable to the file given by the user (which may or may not be the same as the first file called)
 
 print txt_again.read # prints the contents of the second file
