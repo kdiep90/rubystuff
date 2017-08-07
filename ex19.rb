426fd0e066a5e37128812360e0aa222ba7bc6c35
kdiep90 <diep90@gmail.com>
2017-08-07 17:08:25 -0500

    Adding user input options

--- ex19.rb
+++ ex19.rb
@@ -1,6 +1,6 @@
 def cheese_and_crackers(cheese_count, boxes_of_crackers)
   puts "You have #{cheese_count} cheeses!"
-  puts "You have #{$boxes_of_crackers} boxes of crackers!"
+  puts "You have #{boxes_of_crackers} boxes of crackers!"
   puts "Man that's enough for a party!"
   puts "Get a blanket. \n\n"
 end
@@ -9,8 +9,10 @@ puts "We can just give the function numbers directly:"
 cheese_and_crackers(20,30)
 
 puts "OR we can use variables from our script:"
-amount_of_cheese = 10
-amount_of_crackers = 50
+print "How much cheese you got? "
+amount_of_cheese = gets.chomp.to_i
+print "How many crackers you got? "
+amount_of_crackers = gets.chomp.to_i
 
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
