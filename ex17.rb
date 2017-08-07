9d758e3580f78cefcbd1791f5dfee1270b35c43e
kdiep90 <diep90@gmail.com>
2017-08-07 16:09:39 -0500

    Merging lines 13 and 14

--- ex17.rb
+++ ex17.rb
@@ -10,9 +10,6 @@ puts "Does the output file exist? #{File.exist?(to_file)}"
 puts "Ready, hit RETURN to continue, CTRL-C to abort."
 $stdin.gets
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
+open(to_file, 'w').write(indata)
 
 puts "Alright, all done."
-
-out_file.close
