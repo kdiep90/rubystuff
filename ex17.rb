abb2e4f615b3461461f89cdeb463e4af53776c03
kdiep90 <diep90@gmail.com>
2017-08-07 16:19:04 -0500

    More shrinking...

--- ex17.rb
+++ ex17.rb
@@ -1,15 +1,9 @@
 from_file, to_file = ARGV
-
-puts "Copying from #{from_file} to #{to_file}"
-
 indata = open(from_file).read
 
-puts "The input file is #{indata.length} bytes long"
-
-puts "Does the output file exist? #{File.exist?(to_file)}"
-puts "Ready, hit RETURN to continue, CTRL-C to abort."
+puts "Copying from #{from_file} to #{to_file}\nThe input file is #{indata.length} bytes long\nDoes the output file exist? #{File.exist?(to_file)}\nReady, hit RETURN to continue, CTRL-C to abort."
 $stdin.gets
 
-open(to_file, 'w').write(indata)
+open(to_file, 'w') {|f| f.write(indata)}
 
 puts "Alright, all done."
