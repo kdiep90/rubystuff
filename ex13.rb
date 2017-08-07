7adea6f5bb98bf5584b10ce92498d961d7970f3e
kdiep90 <diep90@gmail.com>
2017-08-06 20:54:08 -0400

    Using $stdin.gets.chomp

--- ex13.rb
+++ ex13.rb
@@ -2,3 +2,7 @@ first, second, third = ARGV
 puts "Your first variable is: #{first}"
 puts "Your second variable is: #{second}"
 puts "Your third variable is: #{third}"
+
+print "Enter some text: "
+some_string = $stdin.gets.chomp
+puts some_string + " " + first + " " + second + " " + third
