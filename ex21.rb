f2825e910f318d74efea550a104f22abd392d85f
kdiep90 <diep90@gmail.com>
2017-08-08 19:51:43 -0500

    Extra credit (before answer)

--- ex21.rb
+++ ex21.rb
@@ -26,3 +26,10 @@ weight = multiply(90, 2)
 iq = divide(250, 2)
 
 puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+# Extra Credit
+puts "Here's a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+puts "That becomes #{what}. Can you do it by hand?"
