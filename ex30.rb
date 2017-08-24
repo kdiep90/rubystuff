0f410b85a3e8c349a87b2d38146fbf8b2dbcbda6
kdiep90 <diep90@gmail.com>
2017-08-23 21:58:21 -0400

    Made some use of the OR logical operator.

--- ex30.rb
+++ ex30.rb
@@ -1,32 +1,19 @@
-people = 20
+people = 50
 cars = 20
 trucks = 30
 
-# When there are more cars than people we should take the cars
-if cars > people
-  puts "We should take the cars."
-# Otherwise, in the case there are more people than cars, we should not take the cars
-elsif cars < people
-  puts "We should not take the cars."
-# Or else we won't know what to do
+if (cars >= people) || (trucks >= people) || ((cars + trucks) >= people)
+  puts "Let's go for a ride!"
 else
-  puts "We can't decide."
+  puts "We should stay in today."
 end
 
-# When there are more trucks than cars, that's too many trucks to take.
-if trucks > cars
-  puts "That's too many trucks."
-# If there are less trucks than cars, we could take the trucks
-elsif trucks < cars
-  puts "Maybe we could take the trucks."
-# Otherwise we still can't decide what to do.
+if cars >= people # We want to take only cars if possible to save on gas
+  puts "Let's take the cars."
+elsif trucks >= people # If we don't have enough cars but do have enough trucks for everybody, we can take the trucks.
+  puts "Let's take the trucks."
+elsif (cars + trucks) >= people # Otherwise if we don't have enough of a single kind, but do have enough for everybody, we can take a mix of cars and trucks.
+  puts "Let's take both cars and trucks."
 else
-  puts "We still can't decide."
-
-# Well if there are more people than trucks, we should take the trucks
-if people > trucks
-  puts "Alright, let's just take the trucks."
-# Otherwise let's call it a day and stay in.
-else
-  puts "Fine, let's just stay home then."
+  puts "We don't have enough vehicles for everyone anyway."
 end
