a51a9ec24d3ad0f423f421621d306e9f20181d70
kdiep90 <diep90@gmail.com>
2017-09-19 17:42:53 -0400

    corrections to ex35

--- ex35.rb
+++ ex35.rb
@@ -64,26 +64,23 @@ end
 def dead(why)
   puts why, "Good job!"
   exit(0)
+end
 
-  def start
-    puts "You are in a dark room."
-    puts "There is a door to your left and right."
-    puts "Which one do you take?"
+def start
+  puts "You are in a dark room."
+  puts "There is a door to your left and right."
+  puts "Which one do you take?"
 
-    print "> "
-    choice = $stdin.gets.chomp
+  print "> "
+  choice = $stdin.gets.chomp
 
-    if choice == "left"
-      bear_room
-    elsif choice =- "right"
-      cthulhu_room
-    else
-      dead("You stumble around the room until you starve.")
-    end
+  if choice == "left"
+    bear_room
+  elsif choice =- "right"
+    cthulhu_room
+  else
+    dead("You stumble around the room until you starve.")
   end
+end
 
   start
-
-
-
-gold_room
